be4b7964bb4f260f7eff425976a572792d12a7ef
AddisBelayhu <addisgithub@gmail.com>
2025-10-20 10:05:07 +0300

    Initial commit

---
+++ Server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false}))
+app.use(express.static("public"))
+
+
+app.get("/", (req, res) => {
+   // res.send("LalNova Technologies PLC")
+    res.render("homepage")
+   
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.post("/register", (req, res) => {
+    console.log(req.body)
+    res.send("Thank you for filling out the form.")
+})
+
+app.listen(3000)
+
