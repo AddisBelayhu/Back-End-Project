c674ff7727e0d21abd4e27964e144cd936e9a946
AddisBelayhu <addisgithub@gmail.com>
2025-10-21 14:53:57 +0300

    daily update

--- Server.js
+++ Server.js
@@ -1,5 +1,25 @@
 const express = require("express")
+//call the package 
+const bcrypt = require("bcrypt")
 const { errors } = require("web3")
+const db = require("better-sqlite3")("ourApp.db")
+//to increase the database performance do the next one
+db.pragma("journal_mode = WAL") 
+ // database setup starts here
+ const createTables = db.transaction(() => {
+    db.prepare(
+        `
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username STRING NOT NULL UNIQUE,
+            password STRING NOT NULL
+        )
+        `
+    ).run()
+ })
+
+ createTables()
+ // database setup ennds heare
 const app = express()
 
 app.set("view engine", "ejs")
@@ -11,8 +31,6 @@ app.use(function (req, res, next) {
     res.locals.errors = []
     next()
 })
-
-
 
 app.get("/", (req, res) => {
    // res.send("LalNova Technologies PLC")
@@ -39,11 +57,26 @@ if (req.body.username && req.body.username.length < 3) errors.push("Username mus
 if (req.body.username && req.body.username.length > 10) errors.push("Username cannot exceed 10 characters.")
 if (req.body.username && !req.body.username.match(/^[a-zA-Z0-9]+$/))errors.push("Username can only contain letters and numbers")
 
+if (!req.body.password) errors.push("You must provide a Password")
+if (req.body.password && req.body.password.length < 8) errors.push("Password must be at least 8 characters.")
+if (req.body.password && req.body.password.length > 12) errors.push("Password cannot exceed 12 characters.")
+
+
 if (errors.length){
     return res.render("homepage", { errors })
-} else {
-  res.send("Thank you for filling out the form.")
-}    
+} 
+    
+// save the new user into a database
+// to make the password encrypted make shure to install// npm install bcrypt
+const salt = bcrypt.genSaltSync(10)
+req.body.password = bcrypt.hashSync(req.body.password, salt)
+// install a databse called sqllite3, npm install better-sqlite3
+const ourStatement = db.prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+ourStatement.run(req.body.username, req.body.password)
+
+// log the user in by giving them a cookies
+
+res.send("Thank You")
 })
 
 app.listen(3000)
